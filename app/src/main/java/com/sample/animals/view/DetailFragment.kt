3bfef0fd4ac839c68507c3c604487be30e0dfe88
chiafeng79 <chiafeng79@gmail.com>
2020-01-02 16:24:31 +0800

    Palette

--- app/src/main/java/com/sample/animals/view/DetailFragment.kt
+++ app/src/main/java/com/sample/animals/view/DetailFragment.kt
@@ -1,12 +1,18 @@
 package com.sample.animals.view
 
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 
 import com.sample.animals.R
 import com.sample.animals.model.Animal
@@ -42,6 +48,28 @@ class DetailFragment : Fragment() {
         animalLocation.text = animal?.location
         animalLifespan.text = animal?.lifeSpan
         animalDiet.text = animal?.diet
+        animal?.imageUrl?.let {
+            setupBackgroundColor(it)
+        }
+    }
+
+    private fun setupBackgroundColor(url:String){
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>(){
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate(){
+                            val intColor = it?.lightMutedSwatch?.rgb?:0
+                            animalLayout.setBackgroundColor(intColor)
+                        }
+                }
+
+            })
     }
 
 
