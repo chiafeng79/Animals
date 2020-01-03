9ac42247a1ce29e2dd52025e4048e781036d5834
chiafeng79 <chiafeng79@gmail.com>
2020-01-03 15:37:29 +0800

    Injecting the ApiService

---
+++ app/src/main/java/com/sample/animals/di/ApiComponent.kt
@@ -0,0 +1,10 @@
+package com.sample.animals.di
+
+import com.sample.animals.model.AnimalApiService
+import dagger.Component
+
+@Component(modules = [ApiModule::class])
+interface ApiComponent {
+
+    fun inject(service:AnimalApiService)
+}
---
+++ app/src/main/java/com/sample/animals/di/ApiModule.kt
@@ -0,0 +1,25 @@
+package com.sample.animals.di
+
+import com.sample.animals.model.AnimalApi
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+class ApiModule {
+
+    private val BASE_URL = "https://us-central1-apis-4674e.cloudfunctions.net"
+
+    @Provides
+    fun providerAnimalApi():AnimalApi{
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(AnimalApi::class.java)
+    }
+}
